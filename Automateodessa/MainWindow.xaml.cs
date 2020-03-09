a2c7797c23cdadc7cb61b78c34dad2d57be87c79
AnishTiwari <tiwari1999anish@gmail.com>
2020-03-09 23:51:08 +0530

    Add project files.

---
+++ Automateodessa/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows;
+using System;
+
+
+namespace Automateodessa
+{
+    
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+            DataContext = new DbDirectoryItemSource();
+        }
+
+
+        private void CmbDirectory_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            List<string> DbFilesList = new List<string>();
+            string DirectorySelected = CmbDirectory.SelectedItem.ToString();
+            if (DirectorySelected != null)
+            {
+                foreach (string i in Directory.GetFiles(DirectorySelected))
+                {
+                    DbFilesList.Add(Path.GetFileName(i));
+                }
+                CmbDatabasefile.ItemsSource = DbFilesList;
+            }
+            else
+            {
+                CmbDatabasefile.ItemsSource = "Please choose the Directory Path";
+            }
+        }
+        public static async Task CopyFileAsync(string sourceFile, string destinationFile)
+        {
+            using (var sourceStream = new FileStream(sourceFile, FileMode.Open, FileAccess.Read, FileShare.Read, 4092, FileOptions.Asynchronous | FileOptions.SequentialScan))
+            using (var destinationStream = new FileStream(destinationFile, FileMode.CreateNew, FileAccess.Write, FileShare.None, 4092, FileOptions.Asynchronous | FileOptions.SequentialScan))
+                await sourceStream.CopyToAsync(destinationStream);
+        }
+        public void DownloadButton_Click(object sender, RoutedEventArgs e)
+        {
+            var watch = new System.Diagnostics.Stopwatch();
+
+            watch.Start();
+            string SelectedDbFile = CmbDirectory.SelectedItem.ToString() + @"/" + CmbDatabasefile.SelectedItem.ToString();
+            CopyFileAsync(SelectedDbFile, @"D:\Driivers.iso");
+            Output_Window.Text = ConfigurationManager.AppSettings["Username"];
+            //Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
+        }
+      
+    }
+
+    public class DbDirectoryItemSource
+    {
+        public List<string> CmbContent { get; set; }
+
+        public DbDirectoryItemSource()
+        {
+            CmbContent = new List<string>();
+            foreach(var i in Directory.GetDirectories(@"E:/"))
+            {
+               CmbContent.Add(i);
+            }
+        }
+    }
+  
+}
---
+++ Automateodessa/SiteAutomate.xaml.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Automateodessa
+{
+    /// <summary>
+    /// Interaction logic for SiteAutomate.xaml
+    /// </summary>
+    public partial class SiteAutomate : Window
+    {
+        public bool[] checkbool;
+        int flag = 0;
+        public SiteAutomate()
+        {
+            InitializeComponent();
+        }
+
+        private void GetSitesAvailable_Click(object sender, RoutedEventArgs e)
+        {
+            DataGridTextColumn c1 = new DataGridTextColumn();
+            c1.Header = "SiteName";
+            c1.Binding = new Binding("SiteName");
+            c1.Width = 110;
+            SitesAvailableDataGrid.Columns.Add(c1);
+            //DataGridTextColumn c2 = new DataGridTextColumn();
+            //c2.Header = "IsEnabled";
+            //c2.Width = 110;
+            //c2.Binding = new Binding("IsEnabled");
+            //SitesAvailableDataGrid.Columns.Add(c2);
+            //DataGridTextColumn c3 = new DataGridTextColumn();
+            //c3.Header = "StartButton";
+            //c3.Width = 110;
+            //c3.Binding = new Binding("StartButton");
+            //SitesAvailableDataGrid.Columns.Add(c3);
+            //DataGridTextColumn c4 = new DataGridTextColumn();
+            //c4.Header = "StopButton";
+            //c4.Width = 110;
+            //c4.Binding = new Binding("StopButton");
+            //SitesAvailableDataGrid.Columns.Add(c4);
+            List<SitesAvailable> SitesAvailableList = new List<SitesAvailable>();
+            for (int i = 0; i < 5; i++)
+            {
+               
+                SitesAvailableDataGrid.Items.Add(new SitesAvailable() { SiteName = "asd" 
+                    
+                    });
+
+            }
+            checkbool = new bool[5];
+            for(int i=0;i<checkbool.Length;i++)
+            {
+                checkbool[i] = true;
+            }
+
+            
+        }
+
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+       
+          
+            if (flag == 1)
+            {
+
+                Console.WriteLine("sdas");
+            }
+           
+        }
+      
+        private void Stop_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("sd");
+           
+        }
+
+        private void StartBtn_Click(object sender, RoutedEventArgs e)
+        {
+            int v = SitesAvailableDataGrid.SelectedIndex;
+            
+            SitesAvailable sitesAvailable = (SitesAvailable)SitesAvailableDataGrid.SelectedItem;
+            checkbool[v] = false;
+            StopBtn.IsEnabled = false;
+            MessageBox.Show("Start clicked For " + ((SitesAvailable)SitesAvailableDataGrid.SelectedItem).SiteName.ToString());
+
+        }
+
+        private void StopBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Stop clicked For " + ((SitesAvailable)SitesAvailableDataGrid.SelectedItem).SiteName.ToString());
+        }
+
+        private void SitesAvailableDataGrid_Selected(object sender, RoutedEventArgs e)
+        {
+            int v = SitesAvailableDataGrid.SelectedIndex;
+            bool flag = checkbool[v];
+            SitesAvailable sitesAvailable = (SitesAvailable)SitesAvailableDataGrid.SelectedItem;
+            if(!flag)
+            {
+                StopBtn.IsEnabled = false;
+            }
+            else
+            {
+                StopBtn.IsEnabled = true;
+            }
+        }
+    }
+    public class SitesAvailable
+    {
+        private bool IsEnabled = true;
+        public string SiteName { get; set; }
+        public bool isEnabled {
+            get
+            {
+                return IsEnabled;
+            }
+            set
+            {
+                IsEnabled = true;
+            }
+        }
+
+    }
+}
